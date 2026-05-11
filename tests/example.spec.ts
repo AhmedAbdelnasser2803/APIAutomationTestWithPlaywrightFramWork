788367aa4cec82d5406bc536a5b322b4013d0901
Ahmed Abdelnasser Saad <ahmed.abdelnasser2803@gmail.com>
2026-05-12 00:17:33 +0300

    mock API

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
+  page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+    await route.fulfill({
+      body: JSON.stringify(tags),});
+  });
   await page.goto('/');
 });
 
@@ -9,5 +14,12 @@ test('has title', async ({ page }) => {
   
   await page.waitForTimeout(3000)
   await expect(page.locator('.logo-font').first()).toHaveText('conduit');
+
+
 });
 
+
+// page is still ope ned after running testcases 
+test.afterEach(async ({ page }) => {
+  await page.pause();
+} );
