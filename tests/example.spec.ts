9a50399d6bd9774d46fa046129799bde6bd6bd87
Ahmed Abdelnasser Saad <ahmed.abdelnasser2803@gmail.com>
2026-05-11 16:45:51 +0300

    update for the first commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,24 +2,21 @@ import { test, expect } from '@playwright/test';
 import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
-  page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+  // mocking the API response for tags endpoint
+  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
     await route.fulfill({
-      body: JSON.stringify(tags),});
+      body: JSON.stringify(tags),
+    });
   });
   await page.goto('/');
 });
 
 test('has title', async ({ page }) => {
-  // check that  the navigation is working and the title is correct
-  
-  await page.waitForTimeout(3000)
+  // check that the navigation is working and the title is correct
+  await page.waitForTimeout(3000);
   await expect(page.locator('.logo-font').first()).toHaveText('conduit');
-
-
 });
 
-
-// page is still ope ned after running testcases 
 test.afterEach(async ({ page }) => {
   await page.pause();
-} );
+});
